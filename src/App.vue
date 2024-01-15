c6aae8b3a96c64135a18d5c07559601db505f8ca
karimdib <karim.dib2001@gmail.com>
2024-01-15 15:50:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import Project from './components/Projects.vue';
+import axios from 'axios';
+import { store } from '/store.js';
+export default {
+  components: {
+    Project,
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Vue+ Laravel</h1>
+    <Project />
+  </div>
+</template>
+
+<style lang="scss">
+@use './style.scss';
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios';
+import { store } from '/store.js';
+export default {
+    data() {
+        return {
+            projects: store.projects,
+        }
+    }
+    ,
+    methods: {
+        callApi() {
+            axios
+                .get("http://127.0.0.1:8000/api/projects")
+                .then((reply) => {
+                    console.log(reply.data.projects)
+                    this.projects = reply.data.projects
+                    console.log(this.projects)
+                })
+        },
+    },
+    created() {
+        this.callApi()
+    },
+}
+</script>
+
+<template>
+    <div>
+        <div class="container">
+            <h2>ciao sono la pagina projects</h2>
+            <div class="row">
+                <div class="col-3" v-for="project in projects">
+                    <p>{{ project.name }}</p>
+                    <p>{{ project.description }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
